a9e5b70191d2f8c2063a1629da963a09ae2ec2d8
chazzlee <l.chazz00@gmail.com>
2023-02-16 14:47:37 -0500

    add user data access

--- SuggestionAppLibrary/DataAccess/DbConnection.cs
+++ SuggestionAppLibrary/DataAccess/DbConnection.cs
@@ -1,6 +1,34 @@
+using System;
+using Microsoft.Extensions.Configuration;
+
 namespace SuggestionAppLibrary.DataAccess;
 
-public class DbConnection
+public class DbConnection : IDbConnection
 {
+    private readonly IConfiguration _configuration;
+    private readonly IMongoDatabase _db;
+    private string _connectionId = "MongoDB";
+    public string DbName { get; private set; } = String.Empty;
+    public string CategoryCollectionName { get; private set; } = "categories";
+    public string StatusCollectionName { get; private set; } = "statuses";
+    public string UserCollectionName { get; private set; } = "users";
+    public string SuggestionCollectionName { get; private set; } = "suggestions";
+    public MongoClient Client { get; private set; }
+    public IMongoCollection<CategoryModel> CategoryCollection { get; private set; }
+    public IMongoCollection<StatusModel> StatusCollection { get; private set; }
+    public IMongoCollection<UserModel> UserCollection { get; private set; }
+    public IMongoCollection<SuggestionModel> SuggestionCollection { get; private set; }
 
+    public DbConnection(IConfiguration configuration)
+    {
+        _configuration = configuration;
+        Client = new MongoClient(_configuration.GetConnectionString(_connectionId));
+        DbName = _configuration["DatabaseName"] ?? String.Empty;
+        _db = Client.GetDatabase(DbName);
+
+        CategoryCollection = _db.GetCollection<CategoryModel>(CategoryCollectionName);
+        StatusCollection = _db.GetCollection<StatusModel>(StatusCollectionName);
+        UserCollection = _db.GetCollection<UserModel>(UserCollectionName);
+        SuggestionCollection = _db.GetCollection<SuggestionModel>(SuggestionCollectionName);
+    }
 }
---
+++ SuggestionAppLibrary/DataAccess/IUserData.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SuggestionAppLibrary.DataAccess;
+
+public interface IUserData
+{
+    Task CreateUser(UserModel user);
+    Task<UserModel> GetUser(string id);
+    Task<UserModel> GetUserFromAuthentication(string objectId);
+    Task<List<UserModel>> GetUsersAsync();
+    Task UpdateUser(UserModel user);
+}
